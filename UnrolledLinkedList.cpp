036defeb2b5936d4c422a80551576ee7d17dae37
UrekMazino <khoi.do.1711807@hcmut.edu.vn>
2018-10-20 11:34:39 +0700

    initial add method

--- UnrolledLinkedList.cpp
+++ UnrolledLinkedList.cpp
@@ -1,8 +1,30 @@
 #include"UnrolledLinkedList.h"
 
 void UnrolledLinkedList::add(int val) {
-	// TODO
-	
+	// C1-- : Urolled Linked List is empty
+	if(tail == NULL) {
+		head = tail = new Node(nodeSize);
+		tail->add(val);
+		size = numOfNodes = 1;
+		return;
+	}
+	// C2-- : Urolled Linked List is NOT empty
+	// // C2_1 : Unrolled Linked List is full
+	if(tail->isFull()) {
+		// Create a new node
+		Node *newNode = new Node(nodeSize);
+		// Move final half of tail into new node
+		for(int i = 0; i < nodeSize/2; ++i) 
+			newNode->elements[i] = tail->elements[nodeSize - nodeSize/2 + i]
+		tail->numOfNodes = nodeSize - nodeSize/2; 
+		// Add val into new node
+		newNode->add(val);
+		// Adjust tail of Urolled Linked List 
+		tail->next = newNode;
+		tail = newNode;
+	}
+	// // C2_2 : Unrolled Linked List is not full and not empty
+	else tail->add(val);
 }
 
 int UnrolledLinkedList::getAt(int pos) {
